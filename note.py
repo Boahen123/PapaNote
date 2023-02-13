e57ba1c36894a3f8541b5eef6296ab6634c8ced6
Papa Kofi <humblepapa1234@gmail.com>
2023-02-13 12:17:17 +0000

    changes made to note.py

--- note.py
+++ note.py
@@ -6,41 +6,40 @@ from tkinter import filedialog
 import customtkinter
 
 
-                                    
-
 class App(customtkinter.CTk):
     ''' Class for the main window '''
 
     def __init__(self):
         super().__init__()
         self._set_appearance_mode('dark')
-        
+
         # title of the main window
         self.title('PapaNote')
 
         # packing the main window
-        # self.geometry(f"{500}x{500}")
+
         self.rowconfigure(0, minsize=800, weight=1)
         self.columnconfigure(1, minsize=800, weight=1)
-        
+
         # The text area
         self.text_edit = customtkinter.CTkTextbox(self)
         self.text_edit.configure(state='normal')
         self.text_edit.grid(row=0, column=1, sticky="nsew")
-        
-        self.frame_button = tk.Frame(self, relief=tk.SUNKEN, width=100, height=100, bd=3, background='grey')
+
+        self.frame_button = tk.Frame(
+            self, relief=tk.SUNKEN, width=100, height=100, bd=3, background='grey')
         self.frame_button.grid(row=0, column=0, sticky="ns")
-        
+
         # The open button
-        self.button_open = customtkinter.CTkButton(master=self.frame_button, text='OPEN FILE', command=self.open_file)  
+        self.button_open = customtkinter.CTkButton(
+            master=self.frame_button, text='OPEN FILE', command=self.open_file)
         self.button_open.grid(row=0, column=0, padx=5, pady=5)
-        
+
         # The save button
-        self.button_save = customtkinter.CTkButton(master=self.frame_button, text='SAVE AS', command=self.save_file)
+        self.button_save = customtkinter.CTkButton(
+            master=self.frame_button, text='SAVE AS', command=self.save_file)
         self.button_save.grid(row=1, column=0, padx=5)
-      
- 
-        
+
     def open_file(self):
         ''' function to open the file '''
         file_location = filedialog.askopenfilename(
@@ -52,7 +51,7 @@ class App(customtkinter.CTk):
             text = file_input.read()
             self.text_edit.insert(tk.END, text)
         self.title(f'PapaNote - {file_location}')
-        
+
     def save_file(self):
         ''' function to save the file '''
         file_location = filedialog.asksaveasfilename(
@@ -64,7 +63,6 @@ class App(customtkinter.CTk):
             text = self.text_edit.get(1.0, tk.END)
             file_output.write(text)
         self.title(f'PapaNote - {file_location}')
-
 
 
 if __name__ == "__main__":
